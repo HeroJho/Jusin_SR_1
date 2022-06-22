d9a753a075a653f81684fb2512f4879a494f4218
HeroJho <theherojho@naver.com>
2022-06-22 20:25:41 +0900

    기본 파일 정리

---
+++ 220622.Framework/Client/Private/MainApp.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "..\Public\MainApp.h"
+
+
+CMainApp::CMainApp()
+{
+}
+
+
+CMainApp::~CMainApp()
+{
+}
---
+++ 220622.Framework/Client/Public/MainApp.h
@@ -0,0 +1,44 @@
+#pragma once
+
+//class C클래스이름. 
+//{
+//private:
+//	생성자.
+//	소멸자. 
+//public:
+//	Getter
+//public:
+//	Setter
+//public:
+//	함수
+//protected:
+//	변수.
+//protected:
+//	함수
+//private:
+//	변수.
+//private:
+//	함수
+//public:
+//	생성관련된함수(Create)
+//	삭제관련된함수(Free)
+//};
+
+
+class CMainApp
+{
+public:
+	CMainApp();
+	~CMainApp();
+
+public:
+	int			iDAta;
+
+
+
+
+
+
+
+};
+
