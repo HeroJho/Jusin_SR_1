aff0dffa90ca37144670feaa43e0a8bceef3c95c
HeroJho <theherojho@naver.com>
2022-09-06 15:40:10 +0900

    Transform Component

---
+++ 3DPerson/220906_Transform/Client/Private/BackGround.cpp
@@ -0,0 +1,126 @@
+#include "stdafx.h"
+#include "..\Public\BackGround.h"
+#include "GameInstance.h"
+
+CBackGround::CBackGround(ID3D11Device * pDevice, ID3D11DeviceContext * pContext)
+	: CGameObject(pDevice, pContext)
+{
+}
+
+CBackGround::CBackGround(const CBackGround & rhs)
+	: CGameObject(rhs)
+{
+}
+
+HRESULT CBackGround::Initialize_Prototype()
+{
+	return S_OK;
+}
+
+HRESULT CBackGround::Initialize(void * pArg)
+{
+	if (FAILED(Ready_Components()))
+		return E_FAIL;
+
+	XMStoreFloat4x4(&m_ProjMatrix, XMMatrixOrthographicLH(g_iWinSizeX, g_iWinSizeY, 0.f, 1.f));
+
+	XMMatrixLookAtLH();
+	XMMatrixPerspectiveFovLH();
+
+	return S_OK;
+}
+
+void CBackGround::Tick(_float fTimeDelta)
+{
+}
+
+void CBackGround::LateTick(_float fTimeDelta)
+{
+	if (nullptr == m_pRendererCom)
+		return;
+
+
+
+	m_pRendererCom->Add_RenderGroup(CRenderer::RENDER_PRIORITY, this);
+}
+
+HRESULT CBackGround::Render()
+{
+	if (nullptr == m_pVIBufferCom ||
+		nullptr == m_pShaderCom)
+		return E_FAIL;
+
+
+	m_pShaderCom->Set_RawValue("g_WorldMatrix", &XMMatrixIdentity(), sizeof(_float4x4));
+	m_pShaderCom->Set_RawValue("g_ViewMatrix", &XMMatrixIdentity(), sizeof(_float4x4));
+	m_pShaderCom->Set_RawValue("g_ProjMatrix", &XMMatrixIdentity(), sizeof(_float4x4));
+
+	if (FAILED(m_pTextureCom->Set_SRV(m_pShaderCom, "g_DiffuseTexture")))
+		return E_FAIL;
+
+	if (FAILED(m_pShaderCom->Begin(0)))
+		return E_FAIL;
+
+	if (FAILED(m_pVIBufferCom->Render()))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CBackGround::Ready_Components()
+{
+	/* For.Com_Renderer */
+	if (FAILED(__super::Add_Component(LEVEL_STATIC, TEXT("Prototype_Component_Renderer"), TEXT("Com_Renderer"), (CComponent**)&m_pRendererCom)))
+		return E_FAIL;
+
+	/* For.Com_Shader */
+	if (FAILED(__super::Add_Component(LEVEL_STATIC, TEXT("Prototype_Component_Shader_VtxTex"), TEXT("Com_Shader"), (CComponent**)&m_pShaderCom)))
+		return E_FAIL;
+
+	/* For.Com_VIBuffer */
+	if (FAILED(__super::Add_Component(LEVEL_STATIC, TEXT("Prototype_Component_VIBuffer_Rect"), TEXT("Com_VIBuffer"), (CComponent**)&m_pVIBufferCom)))
+		return E_FAIL;
+
+	/* For.Com_Texture */
+	if (FAILED(__super::Add_Component(LEVEL_LOGO, TEXT("Prototype_Component_Texture_Default"), TEXT("Com_Texture"), (CComponent**)&m_pTextureCom)))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+CBackGround * CBackGround::Create(ID3D11Device * pDevice, ID3D11DeviceContext * pContext)
+{
+	CBackGround*		pInstance = new CBackGround(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize_Prototype()))
+	{
+		MSG_BOX(TEXT("Failed To Created : CBackGround"));
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+CGameObject * CBackGround::Clone(void * pArg)
+{
+	CBackGround*		pInstance = new CBackGround(*this);
+
+	if (FAILED(pInstance->Initialize(pArg)))
+	{
+		MSG_BOX(TEXT("Failed To Cloned : CBackGround"));
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+void CBackGround::Free()
+{
+	__super::Free();
+
+	Safe_Release(m_pTextureCom);
+	Safe_Release(m_pVIBufferCom);
+	Safe_Release(m_pShaderCom);
+	Safe_Release(m_pRendererCom);
+
+}
